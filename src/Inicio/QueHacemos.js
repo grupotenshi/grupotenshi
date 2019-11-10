37f55eeed3e12b73235bdf8d2cd2b13c532741c5
Rafael Velásquez <yosoyrafajava@gmail.com>
2019-11-10 16:27:12 -0500

    Iconos

--- src/Inicio/QueHacemos.js
+++ src/Inicio/QueHacemos.js
@@ -7,37 +7,37 @@ class QueHacemos extends React.Component {
           <div className="container">
             <section className="row align-content-center text-center hv-100">
               <article className="wow fadeInLeft  col-lg-4 d-flex flex-column  align-items-center" data-wow-duration="2s">
-                <i className="material-icons">devices</i>
+                <i className="icon-iconos_responsive"></i>
                 <span>Responsive Design</span>
                 <p>Ofrecemos diseños que se ajustan diferentes resoluciones</p>
               </article>
 
               <article className="wow fadeIn col-lg-4 d-flex flex-column  align-items-center" data-wow-duration="3s">
-                <i className="material-icons">code</i>
+                <i className="icon-iconos_progamacion2"></i>
                 <span>Programación</span>
                 <p>Desarrollamos basándonos en las necesidades de tu empresa</p>
               </article>
 
               <article className="wow fadeInRight col-lg-4 d-flex flex-column  align-items-center" data-wow-duration="2s">
-                <i className="material-icons">web</i>
+                <i className="icon-iconos_administracion"></i>
                 <span>Administración</span>
                 <p>Podrás modificar la información de tu página fácilmente</p>
               </article>
 
               <article className="wow fadeInLeft col-lg-4 mt-lg-7 d-flex flex-column align-items-center" data-wow-duration="2s">
-                <i className="material-icons">timelapse</i>
+                <i className="icon-iconos_garantia"></i>
                 <span>Garantía</span>
                 <p>Garantizamos nuestros servicios y tiempo de entrega</p>
               </article>
 
               <article className="wow fadeIn col-lg-4 mt-lg-7 d-flex flex-column  align-items-center" data-wow-duration="3s">
-                <i className="material-icons">trending_up</i>
+                <i className="icon-iconos_comm-mag"></i>
                 <span>Comunnity Manager</span>
                 <p>Promocionamos sus productos y servicios en Google, redes sociales y otros medios </p>
               </article>
 
               <article className="wow fadeInRight col-lg-4 mt-lg-7 d-flex flex-column  align-items-center" data-wow-duration="2s">
-                <i className="material-icons">data_usage</i>
+                <i className="icon-iconos_database2"></i>
                 <span>Estructuras de Bases de Datos</span>
                 <p>Analizamos y diseñamos las estructuras de bases de datos </p>
               </article>
